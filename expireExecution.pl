5cdec64fdd295c3637b5f546aab73eaf47d64e09
Devper Zero <devperzero@outlook.com>
2021-02-24 06:00:40 +0700

    Add expire script

---
+++ expireExecution.pl
@@ -0,0 +1,21 @@
+#!usr/bin/perl -w
+
+# use System::Timeout qw(timeout);
+
+# # timeout("3", "sleep 9"); # timeout exit after 3 seconds
+# system("echo hello");
+
+# print "$@\n";
+
+
+eval {
+    my @array;
+    local $SIG{ALRM} = sub { die "timeout\n" }; # NB: \n required
+    local $/ = "";
+    alarm 3;
+    @array = `TIMEOUT /T 5 /NOBREAK`;
+    alarm 0;
+};
+die $@ if $@ && $@ ne "timeout";
+print "end\n";
+
